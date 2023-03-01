7cced55d232084359d4109a0c9f8350be6fee227
Mitzael17 <misha.fokin.04@mail.ru>
2023-03-01 11:37:36 +0300

    The FilesController was finished.

--- core/controllers/admin/FilesController.php
+++ core/controllers/admin/FilesController.php
@@ -91,7 +91,17 @@ class FilesController extends BaseAdmin
 
             if(empty($files_to_upload) && !empty($not_uploaded)) throw new ApiException('The file(s) can\'t be upload. Rename it or turn on \'auto rename mode\'');
 
-            foreach ($files_to_upload as $file) move_uploaded_file($file['tmp'], $file['to']);
+            $log = 'Uploaded new files: ';
+
+            foreach ($files_to_upload as $file) {
+
+                move_uploaded_file($file['tmp'], $file['to']);
+
+                $log .= $file['to'] . ', ';
+
+            }
+
+            $this->createLog(rtrim($log, ', '));
 
             if(!empty($not_uploaded)) {
 
@@ -100,6 +110,18 @@ class FilesController extends BaseAdmin
                 exit(json_encode(['status' => 'warning', 'message' => "The files can't be uploaded (rename it or turn on 'auto rename mode'): $not_uploaded"]));
 
             }
+
+            exit(json_encode(['status' => 'success']));
+
+        }
+
+        if(isset($_POST['new_directory'])) {
+
+            if(is_dir(UPLOAD_DIR . $_POST['new_directory'])) throw new ApiException('The directory already exists');
+
+            mkdir(UPLOAD_DIR . $_POST['new_directory']);
+
+            $this->createLog('Created a new directory (' . $_POST['new_directory'] . ')');
 
             exit(json_encode(['status' => 'success']));
 
@@ -115,11 +137,43 @@ class FilesController extends BaseAdmin
             'files' => ['necessary']
         ])['files'];
 
+        $message_for_folders = '';
+        $message_for_files = '';
+
         foreach ($files as $file) {
 
-            if(file_exists(UPLOAD_DIR . $file)) unlink(UPLOAD_DIR . $file);
+            if(is_dir(UPLOAD_DIR . $file)) {
+
+                $message_for_folders .= "$file, ";
+
+                $dir = UPLOAD_DIR . $file;
+
+                $it = new \RecursiveDirectoryIterator($dir, \RecursiveDirectoryIterator::SKIP_DOTS);
+                $files = new \RecursiveIteratorIterator($it,
+                    \RecursiveIteratorIterator::CHILD_FIRST);
+
+                foreach($files as $f) {
+                    if ($f->isDir()){
+                        rmdir($f->getRealPath());
+                    } else {
+                        unlink($f->getRealPath());
+                    }
+                }
+                rmdir($dir);
+
+            }
+            elseif(file_exists(UPLOAD_DIR . $file)) {
+
+                $message_for_files .= "$file, ";
+                unlink(UPLOAD_DIR . $file);
+
+            }
+
 
         }
+
+        if(!empty($message_for_folders)) $this->createLog('Deleted the folders: ' . rtrim($message_for_folders, ', '));
+        if(!empty($message_for_files)) $this->createLog('Deleted the files: ' . rtrim($message_for_files, ', '));
 
         exit(json_encode(['status' => 'success']));
 
--- index.php
+++ index.php
@@ -4,7 +4,7 @@ header('Content-type: json/application');
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, PATCH, DELETE, OPTIONS');
+header('Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS');
 
 const ACCESS = true;
 
